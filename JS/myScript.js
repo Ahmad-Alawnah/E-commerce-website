a18464dfc78f4e872246177a3efd60bb6141ce8d
Ahmad Alawnah <aalawneh19@gmail.com>
2019-05-04 22:11:13 +0300

    finished basic form validation JS functions

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,4 +1,5 @@
 function validateSignUpForm(){
+	
 	name = document.getElementById("name").value;
 	email = document.getElementById("email").value;
 	passw = document.getElementById("pass").value;
@@ -410,3 +411,157 @@ function updatePassword(){
 		
 	}
 }
+
+function validatePaymentInfo(){
+	
+	
+	cards = document.getElementsByName("card");
+	card_number = document.getElementById("Cnumber").value;
+	security_code = document.getElementById("sc").value;
+	expiry_date = document.getElementById("expDate").value;
+
+	current = document.getElementById("error");
+	if (current!=null){
+		current.parentNode.removeChild(current);
+	}
+	else setTimeout(deleteError,6000);
+	
+	error = document.createElement("DIV");
+	att = document.createAttribute("class");
+	id = document.createAttribute("id");
+	att.value="Error ";
+	att.value +="text-center";
+	id.value="error";
+	error.setAttributeNode(att);
+	error.setAttributeNode(id);
+	myForm = document.getElementById("paymentInfoForm");
+	myDiv= document.getElementById("paymentInfoDiv");
+	
+		
+	if ((cards[0].checked==false && cards[1].checked==false) || card_number==""|| security_code==""||expiry_date==""){ 
+	
+		errorText = document.createTextNode("Please fill all fields");
+		error.appendChild(errorText);
+		myDiv.insertBefore(error,myForm);	
+		return false;
+	
+	}
+	else if ((cards[0].checked==false && cards[1].checked==false)){
+		
+		errorText = document.createTextNode("Please select a card");
+		error.appendChild(errorText);
+		myDiv.insertBefore(error,myForm);	
+		return false;
+
+	}		
+	else if (isNaN(card_number)){
+		
+		errorText = document.createTextNode("Please enter a valid card number");
+		error.appendChild(errorText);
+		myDiv.insertBefore(error,myForm);	
+		return false;
+	}
+	else if (isNaN(security_code) || security_code <=0 ||security_code >999){
+		
+		errorText = document.createTextNode("Please enter a valid security code");
+		error.appendChild(errorText);
+		myDiv.insertBefore(error,myForm);	
+		return false;
+		
+	}
+	else{
+		
+		//update DB
+		//send email with items
+		alert("THIS IS TEMPORARY, WE WILL SEND AN EMAIL");
+		return true;
+
+	}
+	
+}
+
+function validateLogInForm(){
+	
+	email = document.getElementById("email").value;
+	pass = document.getElementById("pass").value;
+	remember = document.getElementById("remember");
+	
+	
+	//check DB and compare email with pass
+	
+	
+	if (remember.checked){
+		
+		//set cookie for one month
+		
+	}
+	
+}
+
+function applyFilters(){
+	
+	list = document.getElementById("sort");
+	sort_type = list.options[list.selectedIndex].value;
+	price_range = document.getElementById("priceRange");
+	cateory_name = document.getElementById("categoryName");
+	
+	//get all items in category_name
+	//iterate and select items whithin price_range
+	//sort items based on sort_type
+	//display items (call a function if it exists -might do soon-)
+	
+}
+	
+	
+/******************** END OF VALIDATATION FUNCTIONS (hopefully) ***********************/
+
+/*
+ (QUACK) -I wish I was a duck-
+*/
+
+//LOADING STUFF
+
+function loadItems(items,container){ //items is an array than contains items' IDs, and container is the id of the parent
+	
+	/*
+		
+		for each ID in items do:
+			create element DIV with class col and item
+			create element A that links to the items' page
+			create element img with src = picture of the item
+			append img to A and A to DIV
+			create element span with class item_detials and create text node that contains name and price of item seperated by a <br>
+			append span to DIV
+			append DIV to container
+			
+			//all info about an item is in DB and will be extracted using ID
+	
+	
+	
+		//html code for an item:
+
+			<div class = "item col">
+			  <a href="" class="item_link">
+				<img src="Images/Item.png" alt="item"> <br>
+			  </a>
+			  <span class="item_details"> Name Name Name Name Name Name <br> Price Price </span><br> <br>
+			  <button class="myButton"> Add To Cart </button> <br>
+			</div>
+
+	
+	
+	*/
+	
+}
+
+function loadNewlyArrivals(){
+	
+	//get latest additions from DB
+	container  = document.getElementById("newestArrivals");
+	//loadItems(items,container)	
+	
+}
+
+
+
+
